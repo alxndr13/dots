00026dcd0f3d151292e9f290540e9cf6d1d5cadf
Alexander Huck <alexander.huck@inovex.de>
2023-05-25 10:58:10 +0200

    Update markant-vpn-settings.sh

--- executable_markant-vpn-settings.sh
+++ executable_markant-vpn-settings.sh
@@ -1,2 +1,10 @@
-sudo resolvectl dns tun0 10.1.13.53    
-sudo resolvectl domain tun0 marksys.de
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "first argument must be the device"
+  exit 1
+fi
+
+
+sudo resolvectl dns "$1" 10.1.13.53
+sudo resolvectl domain "$1" marksys.de
