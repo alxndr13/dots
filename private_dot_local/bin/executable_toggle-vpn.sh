2da5c7cd22fe8241d57ddf0ac31480e556a0aea0
Alexander Huck <alexander.huck@inovex.de>
2023-10-11 15:17:55 +0200

    Update .local/bin/toggle-vpn.sh

--- private_dot_local/bin/executable_toggle-vpn.sh
+++ private_dot_local/bin/executable_toggle-vpn.sh
@@ -14,12 +14,12 @@ then
     notify-send -t 5000 "VPN Toggle" "Activated $VPN"
     nmcli con up id "$VPN"
     sleep 4
-    ~/.local/bin/whatsapp_routing_over_home_network.sh
+    ~/.local/bin/routing_over_home_network.sh
 else
     notify-send -t 5000 "VPN Toggle" "Deactivated $VPN"
     nmcli con down id "$VPN"
     sleep 4
-    ~/.local/bin/whatsapp_routing_over_home_network.sh delete
+    ~/.local/bin/routing_over_home_network.sh delete
 fi
 
 exit 0
