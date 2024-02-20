94986d1be6bbf11102b4750a1a119bf6af82d463
Alexander Huck <alexander.huck@inovex.de>
2024-02-20 11:00:02 +0100

    Update .local/bin/check-updates.sh

--- private_dot_local/bin/executable_check-updates.sh
+++ private_dot_local/bin/executable_check-updates.sh
@@ -6,14 +6,14 @@ updates="$(checkupdates)"
 updates_count=$(echo "$updates" | wc -l)
 
 if [[ -z $updates_count ]]; then
-  echo '{"text": "0", "tooltip": "zero updates"}'
+  echo '{"text": "0", "tooltip": "zero updates", "class":"$class"}'
   exit 0
 fi
 
 if grep -q "linux-lts" "$updates"; then
-  echo "{\"text\": \"$updates_count ⚠️\", \"tooltip\": \"$updates\"}"
+  echo "{\"text\": \"$updates_count ⚠️\", \"tooltip\": \"$updates\",\"class\":\"\$class\"}"
   exit 0
 fi
 
-echo "{\"text\": \"$updates_count\", \"tooltip\": \"$updates\"}"
+echo "{\"text\": \"$updates_count\", \"tooltip\": \"$updates\",\"class\":\"\$class\"}"
 exit 0
