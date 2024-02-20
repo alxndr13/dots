cf228f0876d432d7772eca2598a33cd647ab93b2
Alexander Huck <alexander.huck@inovex.de>
2024-02-20 13:37:13 +0100

    Update .local/bin/bt-state.sh

--- private_dot_local/bin/executable_bt-state.sh
+++ private_dot_local/bin/executable_bt-state.sh
@@ -2,7 +2,8 @@
 if [[ $(bluetoothctl show | grep Powered | tr -d '\t' | cut -d " " -f 2) = "yes" ]]; then
   string="ON"
   if bluetoothctl info 60:AB:D2:41:CD:11 | grep Connected | tr -d '[:space:]'| cut -d":" -f 2 | grep yes > /dev/null 2>&1; then
-    string="$string -  Delta"
+    percentage=$(bluetoothctl info 60:AB:D2:41:CD:11 | rg Battery | rg  "\((?P<percentage>\d{2})\)" -or "\$1")
+    string="$string -  Delta ($percentage%)"
   fi
   if bluetoothctl info DA:E8:97:89:F2:96 | grep Connected | tr -d '[:space:]'| cut -d":" -f 2 | grep yes > /dev/null 2>&1; then
     string="$string -  MX Keys Mini"
