6df8b4bea6a92c4bbd4b5e4869a072c24e68b74c
Alexander Huck <alexander.huck@inovex.de>
2024-02-21 11:51:57 +0100

    Update .local/bin/bt-state.sh

--- private_dot_local/bin/executable_bt-state.sh
+++ private_dot_local/bin/executable_bt-state.sh
@@ -4,14 +4,14 @@ string=""
 if [[ $(bluetoothctl show | grep Powered | tr -d '\t' | cut -d " " -f 2) = "yes" ]]; then
   string="󰤁"
   if bluetoothctl info 60:AB:D2:41:CD:11 | grep Connected | tr -d '[:space:]'| cut -d":" -f 2 | grep yes > /dev/null 2>&1; then
-  echo "test"
     headsetpercentage=$(bluetoothctl info 60:AB:D2:41:CD:11 | rg Battery | rg  "\((?P<percentage>\d{2})\)" -or "\$1")
     string="$string -  🎧 ($headsetpercentage%)"
   fi
-  # if bluetoothctl info DC:54:35:A3:35:EB | grep Connected | tr -d '[:space:]'| cut -d":" -f 2 | grep yes > /dev/null 2>&1; then
-  #   percentage=$(bluetoothctl info DC:54:35:A3:35:EB | rg Battery | rg  "\((?P<percentage>\d{2})\)" -or "\$1")
-  #   string="$string -  🐭 ($percentage%)"
-  # fi
+  if bluetoothctl info DC:54:35:A3:35:EB | grep Connected | tr -d '[:space:]'| cut -d":" -f 2 | grep yes > /dev/null 2>&1; then
+    percentage=$(bluetoothctl info DC:54:35:A3:35:EB | rg Battery | rg  "\((?P<percentage>\d{2})\)" -or "\$1")
+    string="$string -  🐭 ($percentage%)"
+  fi
+  echo $string
 else
   echo "󰤂"
 fi
