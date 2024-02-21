7b734174e61d6e94f10cd97947df0b091b8ab5c9
Alexander Huck <alexander.huck@inovex.de>
2024-02-21 11:48:59 +0100

    Update .local/bin/bt-state.sh

--- private_dot_local/bin/executable_bt-state.sh
+++ private_dot_local/bin/executable_bt-state.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $(bluetoothctl show | grep Powered | tr -d '\t' | cut -d " " -f 2) = "yes" ]]; then
-  string="ON"
+  string="󰤁"
   if bluetoothctl info 60:AB:D2:41:CD:11 | grep Connected | tr -d '[:space:]'| cut -d":" -f 2 | grep yes > /dev/null 2>&1; then
     percentage=$(bluetoothctl info 60:AB:D2:41:CD:11 | rg Battery | rg  "\((?P<percentage>\d{2})\)" -or "\$1")
     string="$string -  🎧 ($percentage%)"
@@ -10,5 +10,5 @@ if [[ $(bluetoothctl show | grep Powered | tr -d '\t' | cut -d " " -f 2) = "yes"
     string="$string -  🐭 ($percentage)"
   fi
 else
-  echo "❌"
+  echo "󰤂"
 fi
