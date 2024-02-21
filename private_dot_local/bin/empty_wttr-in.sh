171d6d101d75866c7288a89ff38dfde00f5b396d
Alexander Huck <alexander.huck@inovex.de>
2024-02-21 11:31:43 +0100

    Update .local/bin/wttr-in.sh

--- private_dot_local/bin/empty_wttr-in.sh
+++ private_dot_local/bin/empty_wttr-in.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jo text="$(curl wttr.in/Mannheim?format='%c%t+💧+%h')" tooltip="$(curl wttr.in/Mannheim?format=v2)"
+jo text="$(curl wttr.in/Mannheim?format='%c%t+💧+%h')" tooltip="$(curl wttr.in/Mannheim)"
