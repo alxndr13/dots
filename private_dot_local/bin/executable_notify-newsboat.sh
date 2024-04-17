28626de4e4ccd5a14dc374127516b66d748470d3
Alexander Huck <alexander.huck@inovex.de>
2024-04-17 10:19:20 +0200

    fix: remove linux scripts

--- private_dot_local/bin/executable_notify-newsboat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-notify-send -t 5000 -a newsboat -i "/usr/share/icons/Numix/22/places/folder-gitlab.svg" "Newsboat 🛥️" "$1"
--- private_dot_local/bin/executable_wttr-in.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-jo text="$(curl wttr.in/Mannheim?format='%c%t+💧+%h')" tooltip="$(curl wttr.in/Mannheim?format=4)"
